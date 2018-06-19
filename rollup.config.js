0a138ec7c5ec4dae9f2a57ae87fcdb3b60431247
Adam Argyle <atom@argyleink.com>
2018-06-18 21:23:13 -0700

    init

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from 'rollup-plugin-node-resolve'
+
+export default {
+  input: 'app/index.js',
+  output: {
+    file: 'app/bundle.js',
+    format: 'es'
+  },
+  name: 'Shortstack',
+  plugins: [
+    resolve({
+      jsnext: true,
+    })
+  ],
+  watch: {
+    exclude: ['node_modules/**'],
+  }
+}
