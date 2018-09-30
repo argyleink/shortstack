f6a5ea50c6a2150e63f11c9ec4521102d3b28077
Adam Argyle <atom@argyleink.com>
2018-09-30 09:24:07 -0700

    update to latest cssnext with preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
+const postcssPresetEnv = require('postcss-preset-env')
+
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-cssnext': {
+    'postcss-preset-env': postcssPresetEnv({
       browsers: [
         '>0.25%',
         'not ie 11',
-        'not op_mini all'
+        'not op_mini all',
       ],
-    }
+    }),
   }
 }
