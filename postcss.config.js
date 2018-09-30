4c615a6679c9ea6ba93b4fa804bd4a0e19cb991a
Adam Argyle <atom@argyleink.com>
2018-09-30 09:39:10 -0700

    update to stage 0

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,16 @@
-const postcssPresetEnv = require('postcss-preset-env')
+const postcssPresetEnv  = require('postcss-preset-env')
+const postcssImport     = require('postcss-import')
 
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-preset-env': postcssPresetEnv({
+  plugins: [
+    postcssImport(),
+    postcssPresetEnv({
+      stage: 0,
       browsers: [
         '>0.25%',
         'not ie 11',
         'not op_mini all',
       ],
     }),
-  }
+  ]
 }
