5f92fe6199994099425c20091d2634f693644339
Adam Argyle <atom@argyleink.com>
2019-07-10 23:12:59 -0700

    fixes #6

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 const postcssPresetEnv  = require('postcss-preset-env')
 const postcssImport     = require('postcss-import')
 const postcsseasings    = require('postcss-easings')
+const importUrl         = require('postcss-import-url')
 
 module.exports = {
   plugins: [
     postcsseasings(),
+    importUrl(),
     postcssImport(),
     postcssPresetEnv({
       stage: 0,
