85896df3977fc558e961554702353ad8c44c87bd
Adam Argyle <atom@argyleink.com>
2019-11-27 22:24:14 -0800

    update folder structure, breaking changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,5 @@
 // import Whatever from 'https://unpkg.com/whatever'
 // import Whatever from 'whatever'
 
-import { default as index_css }  from './index.css'
-
-console.info(index_css)
+// import { default as index_css }  from './css/index.css'
+// console.info(index_css)
