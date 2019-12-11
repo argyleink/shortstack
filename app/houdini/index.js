2b13418aa64f7e4e5173a5c243a5e41324d643d8
Adam Argyle <argyle@google.com>
2019-12-11 11:10:10 -0800

    simplified registrar

--- app/houdini/index.js
+++ app/houdini/index.js
@@ -1,11 +1,11 @@
 (async function() {
   const registrar = new Set([
-    { name: 'Painty', path: 'painty.worklet.js'}
+    'painty.worklet.js',
   ])
 
   if (CSS['paintWorklet'] === undefined)
     await import('https://unpkg.com/css-paint-polyfill/dist/css-paint-polyfill.js')
 
-  registrar.forEach(({path}) =>
-    CSS.paintWorklet.addModule(`/houdini/${path}`))
+  registrar.forEach(worklet =>
+    CSS.paintWorklet.addModule(`/houdini/${worklet}`))
 })()
