84fb6028056e4aa35f8a9c016674e0a724a6e847
Adam Argyle <argyle@google.com>
2019-12-11 10:34:49 -0800

    more params

--- app/houdini/painty.worklet.js
+++ app/houdini/painty.worklet.js
@@ -1,16 +1,27 @@
 export default class Painty {
+  static get inputProperties() {
+    return [
+      '--painty-hue-start',
+      '--painty-step-size',
+      '--painty-step-width',
+    ]
+  }
+
   paint(ctx, bounds, props) {
     const { width:w, height:h } = bounds
-    const start_hue = 100
+
+    const start_hue = parseInt(props.get('--painty-hue-start').toString())
+    const step_size = parseInt(props.get('--painty-step-size').toString())
+    const step_width = parseInt(props.get('--painty-step-width').toString())
 
     for (let y = 0; y < h; y++) {
       for (let x = 0; x < w; x++) {
         ctx.fillStyle = `hsl(
-          ${start_hue + x * 10} 
+          ${start_hue + x * step_size} 
           ${50 + x}% 
           ${50 + y}%
         )`
-        ctx.fillRect(x * 25, y * 25, 25, 25)
+        ctx.fillRect(x * step_width, y * step_width, step_width, step_width)
       }
     }
   }
