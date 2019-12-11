c1583892ceeac7c93cf3974aaace9d600b1878ac
Adam Argyle <argyle@google.com>
2019-12-11 11:43:02 -0800

    tuck away registerprop work for now

--- app/houdini/painty.worklet.js
+++ app/houdini/painty.worklet.js
@@ -12,7 +12,11 @@ registerPaint(paint_name, class Painty {
 
   paint(ctx, bounds, props) {
     const { width:w, height:h } = bounds
-    const [start_hue = 0, step_size = 10, step_width = 25] = this.parseProps(props)
+    const [
+      start_hue = 0, 
+      step_size = 10, 
+      step_width = 25
+    ] = this.parseProps(props)
 
     for (let y = 0; y < h; y++) {
       for (let x = 0; x < w; x++) {
@@ -31,3 +35,24 @@ registerPaint(paint_name, class Painty {
       parseInt(props.get(param).toString()) || undefined)
   }
 })
+
+// CSS.registerProperty({
+//   name: `--painty-hue-start`,
+//   syntax: '<number>',
+//   inherits: false,
+//   initialValue: 0,
+// })
+
+// CSS.registerProperty({
+//   name: `--painty-step-size`,
+//   syntax: '<number>',
+//   inherits: false,
+//   initialValue: 10,
+// })
+
+// CSS.registerProperty({
+//   name: `--painty-step-width`,
+//   syntax: '<number>',
+//   inherits: false,
+//   initialValue: 20,
+// })
