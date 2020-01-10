7c1f4da7b5269fbfcd4159af49762bfe2a615873
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-01-10 10:57:33 -0800

    Closure Compilation (#18)
    * Add some closure compilation
    
    * Ordering

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
 import { terser } from 'rollup-plugin-terser'
+import compiler from '@ampproject/rollup-plugin-closure-compiler'
 import { default as importHTTP } from 'import-http/rollup'
 
 const isProd = process.env.NODE_ENV === 'production'
@@ -38,6 +39,7 @@ const prodConfig = {
       extract: true,
       minimize: { preset: 'default' },
     }),
+    compiler(),
     terser(),
   ]
 }
