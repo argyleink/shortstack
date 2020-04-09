328034b965b2f8935a023f776a5f71064f6500d9
Adam Argyle <atom@argyleink.com>
2020-04-08 22:30:53 -0700

    more inline informative notes about capabilities

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,14 @@
+// (bundle from remote urls) 
 // import Whatever from 'https://unpkg.com/whatever'
+
+// (bundle from npm)
 // import Whatever from 'whatever'
 
+// (bundle from local)
+// import whatever from './whatever.js'
+
+// postCSS processed css-4-js
+// (file as string)
 // import { default as index_css }  from './css/index.css'
-// console.info(index_css)
-console.log('💀🤘')
+
+console.log('💀')
