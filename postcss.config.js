fa68d7445388caa9c1558e0777d4a6f69341c0f5
Adam Argyle <atom@argyleink.com>
2020-04-26 21:57:25 -0700

    minify css for production builds

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,11 @@ const postcssPresetEnv  = require('postcss-preset-env')
 const postcssImport     = require('postcss-import')
 const postcsseasings    = require('postcss-easings')
 const importUrl         = require('postcss-import-url')
+const cssnano           = require('cssnano')
 
-module.exports = {
+const isProd = process.env.NODE_ENV === 'production'
+
+const devConfig = {
   plugins: [
     importUrl(),
     postcssImport({
@@ -15,3 +18,21 @@ module.exports = {
     }),
   ]
 }
+
+const prodConfig = {
+  plugins: [
+    importUrl(),
+    postcssImport({
+      path: 'app/css',
+    }),
+    postcsseasings(),
+    postcssPresetEnv({
+      stage: 0,
+    }),
+    cssnano({
+      preset: 'default'
+    }),
+  ]
+}
+
+module.exports = isProd ? prodConfig : devConfig
