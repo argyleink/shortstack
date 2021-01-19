9db87bd2a2362760811606598e939c0f8f817af1
Adam Argyle <argyle@google.com>
2021-01-19 12:14:39 -0800

    optimizing for the future pathways of wmr css

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,8 @@
 const postcssPresetEnv  = require('postcss-preset-env')
-const postcssImport     = require('postcss-import')
 const postcsseasings    = require('postcss-easings')
-const importUrl         = require('postcss-import-url')
-const cssnano           = require('cssnano')
 
-const dev = {
+module.exports = {
   plugins: [
-    importUrl(),
-    postcssImport({
-      path: 'app/css',
-    }),
     postcsseasings(),
     postcssPresetEnv({
       stage: 0,
@@ -21,28 +14,3 @@ const dev = {
     }),
   ]
 }
-
-const prod = {
-  plugins: [
-    importUrl(),
-    postcssImport({
-      path: 'app/css',
-    }),
-    postcsseasings(),
-    postcssPresetEnv({
-      stage: 0,
-      features: {
-        'logical-properties-and-values': false,
-        'prefers-color-scheme-query': false,
-        'gap-properties': false,
-      },
-    }),
-    cssnano({
-      preset: 'default'
-    }),
-  ]
-}
-
-module.exports = process.env.NODE_ENV === 'production' 
-  ? prod 
-  : dev
